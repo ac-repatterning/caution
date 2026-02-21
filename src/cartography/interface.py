0d4e6b15e83db3482d94aa8609f9b9f81f69af26
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-21 20:58:35 +0000

    Update src/cartography/interface.py: In Progress

--- src/cartography/interface.py
+++ src/cartography/interface.py
@@ -17,21 +17,19 @@ class Interface:
     """
 
     def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters,
-                 instances: pd.DataFrame):
+                 frame: pd.DataFrame):
         """
 
         :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
                           Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
         :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
                               name, buckets, etc.
-        :param instances: A frame of metrics per gauge instance, and with respect to time; in the
-                          latter case, 1 time point (0.25 hours), 4 time points (1 hour), etc.
+        :param frame: A frame of metrics per gauge instance.
         """
 
         self.__connector = connector
         self.__s3_parameters = s3_parameters
-
-        self.__instances = instances
+        self.__frame = frame
 
     def __get_coarse_boundaries(self) -> geopandas.GeoDataFrame:
         """
@@ -46,14 +44,13 @@ class Interface:
 
         return coarse
 
-    def __get_data(self, points: int) -> geopandas.GeoDataFrame:
+    def __get_data(self) -> geopandas.GeoDataFrame:
         """
 
-        :param points: 1 -> 0.25 hours, 4 -> 1 hour, etc.
         :return:
         """
 
-        values = self.__instances.copy().loc[self.__instances['points'] == points, :]
+        values = self.__frame
         data = geopandas.GeoDataFrame(
             values,
             geometry=geopandas.points_from_xy(values['longitude'], values['latitude'])
@@ -74,17 +71,15 @@ class Interface:
 
         coarse = self.__get_coarse_boundaries()
 
-        for points in self.__instances['points'].unique():
+        __data = self.__get_data()
+        logging.info(__data)
 
-            __data = self.__get_data(points=points)
-            logging.info(__data)
+        limits = __data.copy()[['catchment_id', 'latest']].groupby(
+            by=['catchment_id']).aggregate(lower=('latest', 'min'), upper=('latest', 'max'))
+        logging.info(limits)
 
-            limits = __data.copy()[['catchment_id', 'latest']].groupby(
-                by=['catchment_id']).aggregate(lower=('latest', 'min'), upper=('latest', 'max'))
-            logging.info(limits)
+        data = __data.copy().merge(limits.reset_index(drop=False), how='left', on='catchment_id')
+        logging.info(data)
 
-            data = __data.copy().merge(limits.reset_index(drop=False), how='left', on='catchment_id')
-            logging.info(data)
-
-            src.cartography.illustrate.Illustrate(data=data, coarse=coarse).exc(
-                points=points, n_catchments_visible=n_catchments_visible)
+        src.cartography.illustrate.Illustrate(
+            data=data, coarse=coarse).exc(n_catchments_visible=n_catchments_visible)
