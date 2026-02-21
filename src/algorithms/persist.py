10db127636a6eff07d63bb7d02f70501e2661c7e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-21 20:08:55 +0000

    Update src/algorithms/persist.py

--- src/algorithms/persist.py
+++ src/algorithms/persist.py
@@ -30,28 +30,34 @@ class Persist:
         self.__configurations = config.Config()
         self.__objects = src.functions.objects.Objects()
 
-    def __get_nodes(self, sign: int) -> dict | list[dict]:
+    def __get_nodes(self, direction: int) -> dict | list[dict]:
         """
 
-        :param sign: {-1, +1}
+        :param direction: {-1, +1}
         :return:
         """
 
-        frame: pd.DataFrame = self.__frame.copy().loc[self.__frame['sign'] == sign, :]
+        frame: pd.DataFrame = self.__frame.copy().loc[self.__frame['direction'] == direction, :]
+        if frame.empty:
+            return {}
+
         nodes = src.algorithms.disaggregates.Disaggregates(frame=frame)()
 
         return nodes
 
-    def __persist(self, nodes: dict| list[dict], sign: int):
+    def __persist(self, nodes: dict| list[dict], direction: int):
         """
 
         :param nodes:
-        :param sign:
+        :param direction:
         :return:
         """
 
+        if not nodes:
+            return f'{self.__names.get('direction')}: empty'
+
         return self.__objects.write(
-            nodes=nodes, path=os.path.join(self.__configurations.points_, f'{self.__names.get(sign)}.json'))
+            nodes=nodes, path=os.path.join(self.__configurations.points_, f'{self.__names.get(direction)}.json'))
 
     def exc(self):
         """
@@ -61,8 +67,8 @@ class Persist:
 
         # Negatives & Positives
         computations = []
-        for sign in self.__names.keys():
-            nodes = self.__get_nodes(sign=sign)
-            message = self.__persist(nodes=nodes, sign=sign)
+        for direction in self.__names.keys():
+            nodes = self.__get_nodes(direction=direction)
+            message = self.__persist(nodes=nodes, direction=direction)
             computations.append(message)
         logging.info(computations)
