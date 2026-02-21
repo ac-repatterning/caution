151069cfce750bf2e9c366892495026c820b40de
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-21 22:18:08 +0000

    Update src/cartography/illustrate.py: In Progress

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -80,8 +80,9 @@ class Illustrate:
                 data = instances.to_crs(epsg=3857),
                 name=f'{parcel.catchment_name}',
                 marker=folium.CircleMarker(
-                    radius=22.5, weight=4, color=colours(parcel.decimal),
-                    fillColor=colours(parcel.decimal), fill_opacity=0.65),
+                    radius=22.5, weight=4, stroke=False, fill=True,
+                    color=colours(parcel.decimal)
+                ),
                 tooltip=folium.GeoJsonTooltip(
                     fields=['latest', 'maximum', 'median', 'station_name', 'river_name', 'catchment_name'],
                     aliases=['latest (mm/hr)', 'maximum (mm/hr)', 'median (mm/hr)', 'Station', 'River/Water', 'Catchment']),
@@ -89,12 +90,13 @@ class Illustrate:
                     "fillOpacity": custom.f_opacity(feature['properties']['latest'],
                                                     lower=feature['properties']['lower'],
                                                     upper=feature['properties']['upper']),
-                    "opacity": custom.f_opacity(feature['properties']['latest'],
-                                                lower=feature['properties']['lower'],
-                                                upper=feature['properties']['upper']),
+                    "fillColor": custom.f_fill_colour(feature['properties']['latest']),
+                    # "opacity": custom.f_opacity(feature['properties']['latest'],
+                    #                             lower=feature['properties']['lower'],
+                    #                             upper=feature['properties']['upper']),
                     "radius": custom.f_radius(feature['properties']['latest']),
-                    "stroke": custom.f_stroke(feature['properties']['latest']),
-                    "fill": custom.f_fill(feature['properties']['latest'])
+                    # "stroke": custom.f_stroke(feature['properties']['latest']),
+                    # "fill": custom.f_fill(feature['properties']['latest']),
                 },
                 zoom_on_click=True,
                 show=show
