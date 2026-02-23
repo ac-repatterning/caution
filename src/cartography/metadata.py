c8955534d9e03fc4418f37da7198f27460f6a168
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-23 15:36:05 +0000

    Update src/cartography/metadata.py

--- src/cartography/metadata.py
+++ src/cartography/metadata.py
@@ -19,14 +19,14 @@ class Metadata:
         return """
                 function(feature, layer) {
                     layer.bindTooltip(
-                        '<b>' + feature.properties.station_name + '</b><br>' +
+                        '<b>' + feature.properties.station_name + '</b><br><br>' +
                         'Latest: ' + feature.properties.latest.toFixed(4) + ' mm/hr<br>' +
                         'Maximum: ' + feature.properties.maximum.toFixed(4) + ' mm/hr<br>' +
                         'Median: ' + feature.properties.median.toFixed(4) + ' mm/hr<br>' +
                         'River/Water: ' + feature.properties.river_name + '<br>' +
                         'Catchment: ' + feature.properties.catchment_name + '<br><br>' +
-                        '<b>PERIOD:</b> ' + new Date(feature.properties.p_beginning).toISOString() + ' &Rarr; ' +
-                        new Date(feature.properties.p_ending).toISOString() + '.<br>'
+                        '<b>PERIOD:</b> ' + new Date(feature.properties.p_beginning).toISOString() + 
+                        ' <b>↠</b> ' + new Date(feature.properties.p_ending).toISOString() + '.<br>'
                     );
                 }
                 """
