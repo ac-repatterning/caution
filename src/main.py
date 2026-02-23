026deea0c3827c4fe4356510b967442a516ad00d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-23 15:08:19 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -32,8 +32,8 @@ def main():
     src.menu.interface.Interface().exc(reference=reference)
 
     # Transferring calculations to an Amazon S3 (Simple Storage Service) bucket
-    # src.transfer.interface.Interface(
-    #     connector=connector, service=service, s3_parameters=s3_parameters).exc()
+    src.transfer.interface.Interface(
+        connector=connector, service=service, s3_parameters=s3_parameters).exc()
 
     # Cache
     src.functions.cache.Cache().exc()
