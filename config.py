cd969e895c2eeafd220bf46f2aea1a7078fd10c5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-23 11:00:41 +0000

    Update config.py

--- config.py
+++ config.py
@@ -17,19 +17,19 @@ class Config:
         """
 
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
-        self.risks_ = os.path.join(self.warehouse, 'risks')
-        self.points_ = os.path.join(self.risks_, 'points')
-        self.menu_ = os.path.join(self.risks_, 'menu')
-        self.maps_ = os.path.join(self.risks_, 'maps')
-        self.series_ = os.path.join(self.risks_, 'series')
+        self.caution_ = os.path.join(self.warehouse, 'caution')
+        self.points_ = os.path.join(self.caution_, 'points')
+        self.menu_ = os.path.join(self.caution_, 'menu')
+        self.maps_ = os.path.join(self.caution_, 'maps')
+        self.series_ = os.path.join(self.caution_, 'series')
 
         # Template
         self.s3_parameters_key = 's3_parameters.yaml'
-        self.arguments_key = 'risks/arguments.json'
-        self.metadata_ = 'risks/external'
+        self.arguments_key = 'caution/arguments.json'
+        self.metadata_ = 'caution/external'
 
         # The storage prefix
-        self.prefix = 'warehouse/risks'
+        self.prefix = 'warehouse/caution'
 
         # Project metadata
         self.project_tag = 'hydrography'
